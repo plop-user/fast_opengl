b1979a1ae7d96bebad070b4c8b2c9e2f9d39b16f
Abhinav <abhinav2002lko@gmail.com>
2026-01-20 20:10:24 +0530

    .

--- shaders/vet.glsl
+++ shaders/vet.glsl
@@ -3,7 +3,8 @@ layout (location=0) in vec3 apos;
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
+out vec4 locdata;
 void main(){
 	gl_Position = projection * view * model * vec4(apos, 1.0);
-
+	locdata = gl_Position;
 }
